9006c46e7bc50bf156dda042549b5d9a3307c2c7
harshveer7828 <harshveer7828@gmail.com>
2025-02-22 13:49:19 +0530

    [Harsh veer singh] Add. Greet with custom attributes feature added with User DTO class and service controller class added✨

--- greetingapp/src/main/java/com/greetingapp/greeting/controller/GreetingServiceController.java
+++ greetingapp/src/main/java/com/greetingapp/greeting/controller/GreetingServiceController.java
@@ -1,9 +1,7 @@
 package com.greetingapp.greeting.controller;
 
 import com.greetingapp.greeting.service.GreetingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("greet/hello")
@@ -18,5 +16,14 @@ public class GreetingServiceController {
     public String sayHello(){
         return greetingService.getGreetingMessage();
     }
+
+
+    // GET request to greet with firstname and lastname
+    @GetMapping("/data")
+    public String greetWithFullName(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName){
+        return greetingService.customGreetingMessage(firstName,lastName);
+    }
+
+
 
 }
